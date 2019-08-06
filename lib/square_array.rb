36468ffbb9b13890698532a78c0c9149b58cd618
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 15:56:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,10 @@
 def square_array(array)
   # your code here
   counter = 0
+  new_numbers = []
   while counter < array.length do
-  puts array[counter] * array[counter]
+  new_numbers[counter] = array[counter] * array[counter]
   counter += 1
   end
+  return new_numbers
 end

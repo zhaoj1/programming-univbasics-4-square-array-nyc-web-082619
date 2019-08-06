b035e68661cbc45124f49b25da402d30cb8890a5
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 15:57:14 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  counter = 0
+  new_numbers = []
+  while counter < array.length do
+  new_numbers[counter] = array[counter] * array[counter]
+  counter += 1
+  end
+  return new_numbers
 end
